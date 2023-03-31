9c7580795032867cebd2cb02232b7dd3f2e49a98
empathictech <105748722+Empathictech@users.noreply.github.com>
2023-03-31 10:24:28 -0400

    initial commit from smoke signal

---
+++ lib/src/constants.dart
@@ -0,0 +1,25 @@
+library empathetech_ss_api;
+
+const String usersPath = 'users';
+const String fcmTokenPath = 'fcmToken';
+const String displayNamePath = 'displayName';
+const String avatarURLPath = 'avatarURL';
+const String defaultDisplayName = 'Anon';
+const String defaultAvatarURL =
+    'https://raw.githubusercontent.com/Empathetech-LLC/smoke_signal/main/assets/app-icon.png';
+
+// Firebase signals
+const String signalsPath = 'signals';
+const String messagePath = 'message';
+const String activeMembersPath = 'activeMembers';
+const String membersPath = 'members';
+const String ownerPath = 'owner';
+const String memberReqsPath = 'memberReqs';
+
+// Firebase functions
+const String sendPushFunc = 'sendPush';
+const String tokenSeparator = ';me;';
+
+const String tokenData = 'tokens';
+const String titleData = 'title';
+const String bodyData = 'body';
