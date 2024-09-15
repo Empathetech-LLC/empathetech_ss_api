7cc0be4f454df853e78ada468c582607a53eae0c
empathictech <105748722+empathictech@users.noreply.github.com>
2024-09-15 14:38:26 -0400

    comments

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -1,25 +1,57 @@
 library empathetech_ss_api;
 
+/// 'users'
 const String usersPath = 'users';
+
+/// 'fcmToken'
 const String fcmTokenPath = 'fcmToken';
+
+/// 'displayName'
 const String displayNamePath = 'displayName';
+
+/// 'avatarURL'
 const String avatarURLPath = 'avatarURL';
+
+/// 'Anon'
 const String defaultDisplayName = 'Anon';
+
+/// 'https://raw.githubusercontent.com/Empathetech-LLC/smoke_signal/main/assets/app-icon.png'
 const String defaultAvatarURL =
     'https://raw.githubusercontent.com/Empathetech-LLC/smoke_signal/main/assets/app-icon.png';
 
-// Firebase signals
+// Firebase signals //
+
+/// 'signals'
 const String signalsPath = 'signals';
+
+/// 'message'
 const String messagePath = 'message';
+
+/// 'activeMembers'
 const String activeMembersPath = 'activeMembers';
+
+/// 'members'
 const String membersPath = 'members';
+
+/// 'owner'
 const String ownerPath = 'owner';
+
+/// 'memberReqs'
 const String memberReqsPath = 'memberReqs';
 
-// Firebase functions
+// Firebase functions //
+
+/// 'sendPush'
 const String sendPushFunc = 'sendPush';
+
+/// ';me;'
 const String tokenSeparator = ';me;';
 
+/// 'tokens'
 const String tokenData = 'tokens';
+
+/// 'title'
 const String titleData = 'title';
+
+/// 'body'
 const String bodyData = 'body';
